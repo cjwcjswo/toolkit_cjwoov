77dab3a08d504fe52f8d8a7cbf4d44b4b9d1c8df
cjwoov <chlwlsdn37@gmail.com>
2023-11-17 02:21:49 +0900

    - 구글 애드센스 적용 안되는거 다시 수정

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,6 +1,5 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import Script from 'next/script'
 
 import '../styles.css';
 
@@ -15,7 +14,7 @@ export default function MyApp({ Component, pageProps }) {
                     content="여러가지 유용한 툴들을 모은 비밀 공간입니다.😘"
                 />
                 <title>CJWOOV의 도구상자⚒️</title>
-                <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3369836132423900" crossOrigin="anonymous"/>
+                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3369836132423900" crossorigin="anonymous"/>
             </Head>
             <Link href="/" className="noStyle">
                 <div className="header">
