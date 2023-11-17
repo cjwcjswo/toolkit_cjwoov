4d393a983085bc982f8e05dffa39d18b6dc80510
CJWOOV-1\cjwoov <chlwlsdn37@gmail.com>
2023-11-17 13:04:54 +0900

    - 애드센스 스크립트 롤백

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,6 +1,5 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import Script from 'next/script';
 
 import '../styles.css';
 
@@ -15,10 +14,7 @@ export default function MyApp({ Component, pageProps }) {
                     content="여러가지 유용한 툴들을 모은 비밀 공간입니다.😘"
                 />
                 <title>CJWOOV의 도구상자⚒️</title>
-                <Script async 
-                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3369836132423900" 
-                crossOrigin="anonymous"
-                strategy="afterInteractive"/>
+                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3369836132423900" crossorigin="anonymous"/>
             </Head>
             <Link href="/" className="noStyle">
                 <div className="header">
